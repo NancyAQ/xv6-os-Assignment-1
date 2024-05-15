b3084cfe7f479cfa3b7ef9a0577f62fa0afbaaab
NancyAQ <nancyabuq@gmail.com>
2024-05-15 19:32:29 +0300

    added full impl for mem test with mem alloc task2

--- user/memsize_test.c
+++ user/memsize_test.c
@@ -6,6 +6,12 @@ int
 main(int argc, char *argv[])
 {
   int i =memsize();
-  fprintf(1,"current process size is %d",i);
+  fprintf(1,"current process size is %d \n",i);
+  int *help_arr=(int*) malloc(20*1024);
+  int j =memsize();
+  fprintf(1,"current size process after alloc is %d \n",j);
+  free(help_arr);
+  int z =memsize();
+  fprintf(1,"current size process after freeing mem is %d \n",z);
   exit(0);
 }
